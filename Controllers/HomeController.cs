bc4dd309fc161fa6592723775c2416199aafe259
Esau Silva <esausilva85@gmail.com>
2019-12-19 11:55:10 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnet_core_mvc_react.Models;
+
+namespace dotnet_core_mvc_react.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("error/404")]
+        public IActionResult NotFoundPage() {
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# .NET Core MVC with React
+
+...
